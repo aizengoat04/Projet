c319f4ba2c96f1eb73f95909031a3117618dd6ae
aizengoat04 <an.kevin3104@gmail.com>
2024-12-13 21:10:25 +0100

    Passager.java

--- src/main/java/compagnie/Passager.java
+++ src/main/java/compagnie/Passager.java
@@ -13,20 +13,20 @@ public class Passager extends Personne {
         this.reservations = new ArrayList<>();
     }
 
-    // Ajouter une réservation
+
     public void reserverVol(Vol vol) {
         Reservation reservation = new Reservation(this, vol);
         reservations.add(reservation);
         System.out.println("Réservation effectuée pour le vol " + vol.getNumeroVol());
     }
 
-    // Annuler une réservation
+
     public void annulerReservation(int numeroReservation) {
         reservations.removeIf(reservation -> reservation.getNumeroReservation() == numeroReservation);
         System.out.println("Réservation annulée : " + numeroReservation);
     }
 
-    // Obtenir toutes les réservations
+
     public List<Reservation> obtenirReservations() {
         return reservations;
     }
